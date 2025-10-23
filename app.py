53b81dba03cda5ac73093221c5663e7c5c11c69a
Konrad Krzyzkowiak <20147441@tafe.wa.edu.au>
2025-10-23 15:03:49 +0800

    init: initialize the branch and create buttons for 2 players or AI bot game

--- app.py
+++ app.py
@@ -28,7 +28,11 @@ def check_draw():
 def index():
     winner = check_winner()
     draw = check_draw()
-    return render_template('index.html', board=board, current_player=current_player, winner=winner, draw=draw)
+    return render_template('index.html',
+                           board=board,
+                           current_player=current_player,
+                           winner=winner,
+                            draw=draw)
 
 
 @app.route('/play/<int:cell>')
