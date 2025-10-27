ddf83601292f86c7294ab028b6e87e448a82c1d1
Konrad Krzyzkowiak <20147441@tafe.wa.edu.au>
2025-10-27 21:19:58 +0800

    feat: add test and choice files with first ai drafts: winning_move_search and fake_move functions

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def check_draw():
     return ' ' not in board
 
 
+
 @app.route('/')
 def index():
     winner = check_winner()
@@ -34,6 +35,38 @@ def index():
                            winner=winner,
                             draw=draw)
 
+# LINEAR SEARCH
+# def ai_move(symbol):
+#     win_conditions = [
+#         (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Horizontal
+#         (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Vertical
+#         (0, 4, 8), (2, 4, 6)  # Diagonal
+#     ]
+#     # initialize a position int which will be used to get an index
+#     # on the board in case there is a winning move to be made
+#     position = -1
+#     # go through the winning conditions and compare in pairs,
+#     # if there are 2 symbols in a condition and an empty spot, the empty spot
+#     # will be the position to take for the next move
+#     for condition in win_conditions:
+#         if board[condition[0]] == board[condition[1]] != ' ' and board[condition[2]] == ' ':
+#             position = condition[2]
+#         elif board[condition[0]] == board[condition[2]] != ' ' and board[condition[1]] == ' ':
+#             position = condition[1]
+#         elif board[condition[1]] == board[condition[2]] != ' ' and board[condition[0]] == ' ':
+#             position = condition[0]
+#     if position != -1:
+#         board[position] = symbol
+
+
+# @app.route('/play_ai/<int:cell>')
+# def play_ai(cell):
+#     global current_player
+#     if board[cell] == ' ':
+#         board[cell] = current_player
+#         if not check_winner():
+#             ai_move('O')
+#     return redirect(url_for('index'))
 
 @app.route('/play/<int:cell>')
 def play(cell):
---
+++ test.py
@@ -0,0 +1,71 @@
+board = [' ', 'X', ' ',
+         'O', 'X', 'O',
+         ' ', ' ', 'O']
+
+win_combinations = [
+    (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Horizontal
+    (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Vertical
+    (0, 4, 8), (2, 4, 6)  # Diagonal
+]
+# use symbol to allow the function to be used first to check for winning moves
+# and then for blocking moves
+def winning_move_search(symbol):
+    empty_cells = []
+    # go through all cells in board to check if they are empty,
+    # if so, add the index to the list
+    for _ in range(len(board)):
+        if board[_] == ' ':
+            empty_cells.append(_)
+    # for every empty cell, check which winning combinations
+    # is it a part of
+    for empty_cell in empty_cells:
+        for combination in win_combinations:
+            # if the empty cell is in a combination, get the empty cell out of
+            # it and add them to a list
+            if empty_cell in combination:
+                pairs = []
+                for _ in range(len(combination)):
+                    if combination[_] not in empty_cells:
+                        pairs.append(combination[_])
+                # if the "pairs" list is longer than 1 and the symbols on those indexes are matching,
+                # return the empty cell index
+                if len(pairs) > 1 and board[pairs[0]] == board[pairs[1]] == symbol:
+                    return empty_cell
+    return None
+
+def check_winner(some_board):
+    # Winning combinations
+    win_combinations = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Horizontal
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Vertical
+        (0, 4, 8), (2, 4, 6)  # Diagonal
+    ]
+    for combination in win_combinations:
+        if some_board[combination[0]] == some_board[combination[1]] == some_board[combination[2]] != ' ':
+            return some_board[combination[0]]
+    return None
+
+def fake_move(symbol):
+    fake_board = board.copy()
+    empty_cells = []
+    # go through all cells in board to check if they are empty,
+    # if so, add the index to the list
+    for _ in range(len(board)):
+        if board[_] == ' ':
+            empty_cells.append(_)
+    for empty_cell in empty_cells:
+        fake_board[empty_cell] = symbol
+        if check_winner(fake_board) == symbol:
+            return empty_cell
+        else:
+            fake_board[empty_cell] = ' '
+    return None
+
+
+
+print(f'Winning move search for O: {winning_move_search("O")}')
+print(f'Winning move search for X: {winning_move_search("X")}')
+print()
+print(f'Fake move search for O: {fake_move("O")}')
+print(f'Fake move search for X: {fake_move("X")}')
+
